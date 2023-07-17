90b199690b84ccaffe226784394a5d466dc899ba
Eliza Fury <39455919+Efury1@users.noreply.github.com>
2023-07-17 19:07:46 +1000

    Update buttons

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -119,32 +119,64 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   return newRequire;
 })({"main.js":[function(require,module,exports) {
 // Get the progress bar element and the update button
-var progressBar = document.getElementById("progress-bar");
-var updateButton = document.getElementById("update-button");
-var progress = 0; // Set the initial progress value to 0
+var progressfoodBar = document.getElementById("progress-food-bar");
+var updatefoodButton = document.getElementById("update-food-button");
+var foodProgress = 0; // Set the initial progress value to 0
 
 // Update the progress bar with a new progress value
-var updateProgressBar = function updateProgressBar() {
+var updateFoodBar = function updateFoodBar() {
   // Create a new element to represent the progress bar fill
-  progressBar.innerHTML = "<div id='progress-bar-fill'></div>";
+  progressfoodBar.innerHTML = "<div id='progress-food-bar-fill'></div>";
 
   // Get the progress bar fill element from the HTML
-  var progressBarFill = document.getElementById("progress-bar-fill");
+  var progressBarFill = document.getElementById("progress-food-bar-fill");
 
   //const progress1 = Math.min(parseFloat(localStorage.progress || "0"), 100);
-  progressBarFill.style.width = progress + "%";
+  progressBarFill.style.width = foodProgress + "%";
   // Set the width of the progress bar fill based on the progress value
   //progressBarFill.style.width = `${progress}%`;
 };
 
 // Update the progress bar when the button is clicked
-updateButton.addEventListener("click", function () {
-  if (progress === 100) {
+updatefoodButton.addEventListener("click", function () {
+  if (foodProgress === 100) {
     clearInterval(intervalId);
   }
-  if (progress < 100) {
-    progress = progress + 10;
-    updateProgressBar(progress); // Update the progress bar with the new progress value
+  if (foodProgress < 100) {
+    foodProgress = foodProgress + 10;
+    updateFoodBar(foodProgress); // Update the progress bar with the new progress value
+  }
+});
+
+//happiness 
+
+// Get the progress bar element and the update button
+var progressHappinessBar = document.getElementById("progress-happiness-bar");
+var updateHappinessButton = document.getElementById("update-happiness-button");
+var happinessProgress = 0; // Set the initial progress value to 0
+
+// Update the progress bar with a new progress value
+var updateHappinessBar = function updateHappinessBar() {
+  // Create a new element to represent the progress bar fill
+  progressHappinessBar.innerHTML = "<div id='progress-happiness-bar-fill'></div>";
+
+  // Get the progress bar fill element from the HTML
+  var progressBarFill = document.getElementById("progress-happiness-bar-fill");
+
+  //const progress1 = Math.min(parseFloat(localStorage.progress || "0"), 100);
+  progressBarFill.style.width = happinessProgress + "%";
+  // Set the width of the progress bar fill based on the progress value
+  //progressBarFill.style.width = `${progress}%`;
+};
+
+// Update the progress bar when the button is clicked
+updateHappinessButton.addEventListener("click", function () {
+  if (happinessProgress === 100) {
+    clearInterval(intervalId);
+  }
+  if (foodProgress < 100) {
+    happinessProgress = happinessProgress + 10;
+    updateHappinessBar(happinessProgress); // Update the progress bar with the new progress value
   }
 });
 },{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
--- src/main.js
+++ src/main.js
@@ -1,33 +1,69 @@
 
 // Get the progress bar element and the update button
-const progressBar = document.getElementById("progress-bar");
-const updateButton = document.getElementById("update-button");
+const progressfoodBar = document.getElementById("progress-food-bar");
+const updatefoodButton = document.getElementById("update-food-button");
 
-let progress = 0; // Set the initial progress value to 0
+let foodProgress = 0; // Set the initial progress value to 0
 
 // Update the progress bar with a new progress value
-const updateProgressBar = () => {
+const updateFoodBar = () => {
     // Create a new element to represent the progress bar fill
-    progressBar.innerHTML = "<div id='progress-bar-fill'></div>";
+    progressfoodBar.innerHTML = "<div id='progress-food-bar-fill'></div>";
 
     // Get the progress bar fill element from the HTML
-    const progressBarFill = document.getElementById("progress-bar-fill");
+    const progressBarFill = document.getElementById("progress-food-bar-fill");
 
     //const progress1 = Math.min(parseFloat(localStorage.progress || "0"), 100);
-    progressBarFill.style.width = progress + "%";
+    progressBarFill.style.width = foodProgress + "%";
     // Set the width of the progress bar fill based on the progress value
     //progressBarFill.style.width = `${progress}%`;
 }
 
 // Update the progress bar when the button is clicked
-updateButton.addEventListener("click", function () {
+updatefoodButton.addEventListener("click", function () {
 
  
-        if (progress === 100) {
+        if (foodProgress === 100) {
             clearInterval(intervalId); 
         }
-        if (progress < 100) {
-            progress = progress + 10;
-            updateProgressBar(progress); // Update the progress bar with the new progress value
+        if (foodProgress < 100) {
+            foodProgress = foodProgress + 10;
+            updateFoodBar(foodProgress); // Update the progress bar with the new progress value
+        }
+});
+
+
+
+//happiness 
+
+// Get the progress bar element and the update button
+const progressHappinessBar = document.getElementById("progress-happiness-bar");
+const updateHappinessButton = document.getElementById("update-happiness-button");
+
+let happinessProgress = 0; // Set the initial progress value to 0
+
+// Update the progress bar with a new progress value
+const updateHappinessBar = () => {
+    // Create a new element to represent the progress bar fill
+    progressHappinessBar.innerHTML = "<div id='progress-happiness-bar-fill'></div>";
+
+    // Get the progress bar fill element from the HTML
+    const progressBarFill = document.getElementById("progress-happiness-bar-fill");
+
+    //const progress1 = Math.min(parseFloat(localStorage.progress || "0"), 100);
+    progressBarFill.style.width = happinessProgress + "%";
+    // Set the width of the progress bar fill based on the progress value
+    //progressBarFill.style.width = `${progress}%`;
+}
+
+// Update the progress bar when the button is clicked
+updateHappinessButton.addEventListener("click", function () {
+
+        if (happinessProgress === 100) {
+            clearInterval(intervalId); 
+        }
+        if (foodProgress < 100) {
+            happinessProgress = happinessProgress + 10;
+            updateHappinessBar(happinessProgress); // Update the progress bar with the new progress value
         }
 });
